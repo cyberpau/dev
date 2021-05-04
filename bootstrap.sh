0f8d6c6672625ab0f1dacf3bb9629b541477f9f3
cyberpau <johnpaulomataac@gmail.com>
2021-05-04 11:20:28 +0800

    fix shell issues

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Authored by John Paulo Mataac
 
-if [[ $EUID > 0 ]]
+if [[ $EUID -gt 0 ]]
   then echo "Exiting... Please run as root"
+  exit
+fi
+
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+  echo "Starting bootstrap..."
+else
+  echo "Unable to connect to internet"
   exit
 fi
 
@@ -11,7 +18,10 @@ sudo yum update -y
 sudo yum install -y epel-release git traceroute yum-utils
 
 ## IF EXISTS pull dev repo, ELSE clone
-if cd dev; then git pull; else git clone https://github.com/cyberpau/dev.git
+if cd dev; 
+  then git pull; 
+  else git clone https://github.com/cyberpau/dev.git
+fi
 
 ## Install Ansible
 sudo yum install -y ansible
@@ -23,12 +33,11 @@ sudo yum-config-manager \
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo usermod -aG docker "$USER"
 
 ## Install Docker Compose v1.29.1 (April 13, 2021)
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 ## Create Persistent Volumes
-mkdir -p ${HOME}/postgres-data/
-
-
+mkdir -p "${HOME}/postgres-data/"
